efcbf935fe53e6daa7031bd724f6e9670375d5b6
manza33 <bourgois.laetitia@gmail.com>
2019-07-04 16:38:17 +0200

    Nettoyage du code

--- Boat.java
+++ Boat.java
@@ -9,8 +9,6 @@ public class Boat extends Vehicle {
 
     @Override
     public String soStuff() {
-        return  "Je suis " + brand + "et je fais glou glou !";
+        return  "Je suis " + brand + " et je fais glou glou !";
     }
-
-
 }
